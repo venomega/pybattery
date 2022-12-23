b7b229def1ea55902f9d6dbaaf5ec3348584e841
Your Name <you@example.com>
2022-12-23 17:42:48 -0500

    0.1 setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pybattery',
+    author="venomega",
+    author_email="guilleps92@gmail.com",
+    version='0.1',
+    packages=['pybattery', ],
+    requires=[] ],
+    licence='GPL-3',
+    url="https://github.com/venomega/pybattery",
+    download_url="https://github.com/venomega/pybattery/archive/0.1.tar.gz",
+    description="simple unix tool to show every device`s battery on system"
+)
